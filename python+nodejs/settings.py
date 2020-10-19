97472643e3bf25e3310466598d9936cc772cbdce
phimes_work_pc <re0phiems@outlook.com>
2020-10-19 09:19:32 +0800

    update dockerfile

---
+++ python+nodejs/settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-  
+
+search_list = ['北京疫情', '疫情', '特朗普', '中科院']
+
+
+
+LOCAL_MONGO_HOST = '127.0.0.1'
+LOCAL_MONGO_PORT = 27000
+
+REMOTE_MONGO_HOST = '180.76.153.244'
+REMOTE_MONGO_PORT = 27890
